8ad61d3b9712a27cdb607566e7c2b262244dbf90
Dipanshuk1 <dipanshukumar1936@gmail.com>
2025-10-07 19:00:58 +0530

    Added dbConnect.js file

---
+++ server/src/config/dbConnect.js
@@ -0,0 +1,11 @@
+import {connect} from "mongoose";
+
+const dbConnect=async ()=>{ 
+    try{
+        const mongoDbConnection = await connect(process.env.MONGO_URI);
+        console.log(`Database connected : ${mongoDbConnection.connection.host}`)
+    }catch(error){
+        console.log(`Database connection failed ${error}`);
+        process.exit(1);
+    }
+}
