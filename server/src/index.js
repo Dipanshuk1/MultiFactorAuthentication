edc9063934f52e49975f53fbb9b992cdc6a045d6
Dipanshuk1 <dipanshukumar1936@gmail.com>
2025-10-07 18:51:51 +0530

    Updated index.js file

--- server/src/index.js
+++ server/src/index.js
@@ -8,3 +8,17 @@ dotenv.config();
 const app=express();
 
 //Middlewares
+const corsOptions = {
+    origin: ["http://localhost:3001"],
+    credentials: true,
+}
+app.use(express.json({ limit: "100mb"}))
+app.use(express.urlencoded({ limit: "100mb" , extended: true }))
+
+//Routes
+
+//Listen app
+const PORT= process.env.PORT || 7002;
+app.listen(PORT, () =>{
+    console.log(`Server is running on port ${PORT}`);
+});
