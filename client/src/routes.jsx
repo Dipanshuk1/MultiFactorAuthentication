6c6f00dd74fa48f4b58d2541b170b78b4797c3c4
Dipanshuk1 <dipanshukumar1936@gmail.com>
2025-10-08 00:19:23 +0530

    Updated routes.jsx file

--- client/src/routes.jsx
+++ client/src/routes.jsx
@@ -1,11 +1,30 @@
 import { createBrowserRouter } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
+import HomePage from "./pages/HomePage";
+import Verify2FA from "./pages/Verify2FA";
+import Setup2FA from "./pages/Setup2FA";
 import Error from "./pages/Error";
 
-export const router = createBrowserRouter([
+const router = createBrowserRouter([
     {
         path: "/login",
         element: <LoginPage />,
         errorElement: <Error />  
-    }
+    },
+    {
+        path: "/",
+        element: <HomePage />,
+        errorElement: <Error />  
+    },
+    {
+        path: "/setup-2fa",
+        element: <Setup2FA />,
+        errorElement: <Error />  
+    },
+    {
+        path: "/verify-2fa",
+        element: <Verify2FA />,
+        errorElement: <Error />  
+    },
 ])
+export default router; 
