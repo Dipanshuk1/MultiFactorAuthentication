54057fcc0832d8b5506974db7a249388123bc395
Dipanshuk1 <dipanshukumar1936@gmail.com>
2025-10-08 00:15:23 +0530

    Added routes.jsx file

---
+++ client/src/routes.jsx
@@ -0,0 +1,11 @@
+import { createBrowserRouter } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+import Error from "./pages/Error";
+
+export const router = createBrowserRouter([
+    {
+        path: "/login",
+        element: <LoginPage />,
+        errorElement: <Error />  
+    }
+])
