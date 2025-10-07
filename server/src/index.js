c5a92e80ab7eeb4c04d424582e8a449b04a563e8
Dipanshuk1 <dipanshukumar1936@gmail.com>
2025-10-07 21:48:54 +0530

    Updated index.js file

--- server/src/index.js
+++ server/src/index.js
@@ -1,10 +1,16 @@
-import express from "express"
-import session from "express-session"
-import passport from "passport"
-import dotenv from "dotenv"
-import cors from "cors"
+import express from "express";
+import session from "express-session";
+import passport from "passport";
+import dotenv from "dotenv";
+import cors from "cors";
+
+import dbConnect from "./config/dbConnect.js";
+import authRoutes from "./routes/authRoutes.js";
+import "./config/passportConfig.js";
 
 dotenv.config();
+dbConnect();
+
 const app=express();
 
 //Middlewares
@@ -12,10 +18,23 @@ const corsOptions = {
     origin: ["http://localhost:3001"],
     credentials: true,
 }
-app.use(express.json({ limit: "100mb"}))
-app.use(express.urlencoded({ limit: "100mb" , extended: true }))
+app.use(cors(corsOptions));
+app.use(express.json({ limit: "100mb"}));
+app.use(express.urlencoded({ limit: "100mb" , extended: true }));
+app.use(session({
+    secret: process.env.SESSION_SECRET || "secret",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 60000 * 60,
+    }
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 //Routes
+app.use("/api/auth", authRoutes);
 
 //Listen app
 const PORT= process.env.PORT || 7002;
