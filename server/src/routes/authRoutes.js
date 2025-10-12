0d9e9810e36bbdf7a907a54f21ed0b9bc7c3511e
Dipanshuk1 <dipanshukumar1936@gmail.com>
2025-10-13 01:40:30 +0530

    Updated authRoutes.js file

--- server/src/routes/authRoutes.js
+++ server/src/routes/authRoutes.js
@@ -15,7 +15,7 @@ const router = Router();
 //Registration
 router.post("/register", register);
 //Login Route
-router.post("/login", login);
+router.post("/login",passport.authenticate("local"), login);
 //Auth Status Route
 router.get("/status", authStatus);
 //Logout Route
