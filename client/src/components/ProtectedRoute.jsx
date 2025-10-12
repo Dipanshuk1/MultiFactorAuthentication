35786598e9c19e2552e331e565f7d9da25d6e411
Dipanshuk1 <dipanshukumar1936@gmail.com>
2025-10-12 14:09:14 +0530

    Updated ProtectedRoute.jsx file

--- client/src/components/ProtectedRoute.jsx
+++ client/src/components/ProtectedRoute.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 import { Navigate, Outlet } from "react-router-dom";
+import { useSession } from "../context/SessionContext";
 
 const ProtectedRoute = () => {
-    const isLoggedIn = true;
+    const {isLoggedIn , loading} = useSession();
+    console.log("The Logged In: ", isLoggedIn);
+    if(loading) {
+        return <div>Loading...</div>;
+    }
     return isLoggedIn ? <Outlet /> : <Navigate to="/login" />;
 };
 
